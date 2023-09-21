18712be50012bbed14d086637ba8c36c80acef07
Sean Rodman <srodman7689@gmail.com>
2023-09-21 14:28:34 -0500

    Remove migrations.

--- db/migrations/000001_create_receipts_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS receipts (
-	id uuid DEFAULT gen_random_uuid(),
-	retailer text NOT NULL,
-	purchaseDate date NOT NULL,
-	purchaseTime time NOT NULL,
-	total text NOT NULL,
-
-	PRIMARY KEY (id)
-);
--- db/migrations/000002_create_receipt_items_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS receipt_items (
-	id uuid DEFAULT gen_random_uuid(),
-	shortDescription text NOT NULL,
-	price text NOT NULL,
-
-	PRIMARY KEY (id)
-);
